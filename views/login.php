b6a5b6303c078c4de6760aef7d6f84cc231fc294
Elvis Bui <elvisbui.ed@gmail.com>
2019-12-26 20:21:42 -0500

    Created basic login page

--- views/login.php
+++ views/login.php
@@ -2,7 +2,13 @@
     include '../partials/header.php'
 ?>
 
-<h1>This is the Login Page</h1>
+<h1>Police Reporting System - Login</h1>
+<form action="POST">
+    <input type="text" name="username" placeholder="Username">
+    <input type="text" name="password" placeholder="password">
+    <button>Submit</button>
+</form>
+<a href="../index.php">Back to Home</a>
 
 <?php
     include '../partials/footer.php'
