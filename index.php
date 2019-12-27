d4698a48d1f308f890e4e7f8d323caece33aceed
Elvis Bui <elvisbui.ed@gmail.com>
2019-12-26 20:07:05 -0500

    Initial Files

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+    include 'partials/header.php'
+?>
+    <h1>Welcome to the Police Reporting System</h1>
+    <ul>
+        <li><a href="login.php">Login</a></li>
+        <li><a href="register.php">Register</a></li>
+    </ul>
+</body>
+</html>
+
+<?php
+    include 'partials/footer.php'
+?>
