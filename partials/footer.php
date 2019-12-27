340e95ff2ec6691ccdf2633cc211c4d35ec50e4e
Elvis Bui <elvisbui.ed@gmail.com>
2019-12-26 21:15:13 -0500

    Removed redundant code

--- partials/footer.php
+++ partials/footer.php
@@ -5,7 +5,6 @@
 <script type="text/javascript">
     $(document).ready(function () {
         var url = window.location;
-        $('ul.navbar-nav a[href="'+ url +'"]').parent().addClass('active');
         $('ul.navbar-nav a').filter(function() {
              return this.href == url;
         }).parent().addClass('active');
