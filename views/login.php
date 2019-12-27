7ce046f3ec3698d22f4b8ee9907628a7aa88ddd6
Elvis Bui <elvisbui.ed@gmail.com>
2019-12-26 20:34:18 -0500

    Slightly Edited Layout

--- views/login.php
+++ views/login.php
@@ -1,15 +1,22 @@
 <?php
     include '../partials/header.php'
 ?>
-
-<h1>Police Reporting System - Login</h1>
-<form action="POST">
-    <input type="text" name="username" placeholder="Username">
-    <input type="text" name="password" placeholder="password">
-    <button>Submit</button>
-</form>
-<a href="../index.php">Back to Home</a>
-
+<div class="container">
+    <div class="text-center mt-3">
+        <h1>Police Reporting System - Login</h1>
+        <form class=mt-3 action="POST">
+            <div class="form-group">
+                <input type="text" name="username" placeholder="Username">
+            </div>
+            <div class="form-group">
+                <input type="text" name="password" placeholder="Password">
+            </div>
+            <button type="submit" class="btn btn-default">Submit</button>
+            
+        </form>
+        <a href="../index.php">Back to Home</a>
+    </div>
+</div>
 <?php
     include '../partials/footer.php'
 ?>
