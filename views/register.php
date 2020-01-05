ed2bfcfc30729e9caa9243b1fbb2adebdea1d68e
Elvis Bui <elvisbui.ed@gmail.com>
2020-01-04 21:19:37 -0500

    Added more fields to register form

--- views/register.php
+++ views/register.php
@@ -9,7 +9,16 @@
                 <input type="text" name="username" placeholder="Username">
             </div>
             <div class="form-group">
-                <input type="text" name="password" placeholder="password">
+                <input type="password" name="password" placeholder="Password">
+            </div>
+            <div class="form-group">
+                <input type="text" name="firstName" placeholder="First Name">
+            </div>
+            <div class="form-group">
+                <input type="text" name="lastName" placeholder="Last Name">
+            </div>
+            <div class="form-group">
+                <input type="text" name="auth" placeholder="Auth Code">
             </div>
             <button class="btn btn-default">Submit</button>
         </form>
