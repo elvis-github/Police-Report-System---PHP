890853a2bc89d1061db30c428b938a9461549a64
Elvis Bui <elvisbui.ed@gmail.com>
2020-01-04 20:18:30 -0500

    Included connection file in incidents page

--- connection/connection.php
+++ connection/connection.php
@@ -5,14 +5,4 @@
     $dbname     = 'police_report_system';
 
     $dsn = 'mysql:host=' . $host . ';dbname=' . $dbname;
-
-    $pdo = new PDO($dsn, $user, $password);
-    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
-    $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
-
-    $stmt = $pdo->query('SELECT * FROM incident');
-    while($row = $stmt->fetch()){
-        // var_dump($row);
-        echo $row->Incident_Report . '<br>';
-    }
 ?>
--- views/incidents.php
+++ views/incidents.php
@@ -1,5 +1,16 @@
 <?php
-    include '../partials/header.php'
+    include '../partials/header.php';
+    include '../connection/connection.php';
+
+    $pdo = new PDO($dsn, $user, $password);
+    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
+    $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+
+    $stmt = $pdo->query('SELECT * FROM incident');
+    while($row = $stmt->fetch()){
+        // var_dump($row);
+        echo $row->Incident_Report . '<br>';
+    }
 ?>
     <div class="container">
         <div class="text-center mt-3">
