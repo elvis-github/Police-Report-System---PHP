d13b9790dc20d138fccc5e7c991fcb56b58443d4
Elvis Bui <elvisbui.ed@gmail.com>
2020-01-05 20:55:57 -0500

    Included tutorial file

--- views/register.php
+++ views/register.php
@@ -1,5 +1,30 @@
 <?php
-    include '../partials/header.php'
+    include '../partials/header.php';
+    include '../connection/connection.php';
+
+    if($_SERVER["REQUEST_METHOD"] == "POST"){
+        if(empty(trim($_POST["username"]))){
+            $username_err = "Please enter a username.";
+        } else {
+            $sql = "SELECT Officer_ID
+                    FROM officers
+                    WHERE username = :username";
+            if($stmt = $pdo->prepare($sql)){
+                $stmt->bindParam(":username", $param_username, PDO::PARAM_STR);
+                $param_username = trim($_POST["username"]);
+                if($stmt->execute()){
+                    if($stmt->rowCount() == 1){
+                        $username_err = "This username is already taken.";
+                    } else {
+                        $username = trim($_POST["username"]);
+                    }
+                } else {
+                    echo "OOPS! Something went wrong. Please try again later.";
+                }
+                unset($stmt);
+            }
+        }
+    }
 ?>
 <div class="container">
     <div class="text-center mt-3">
