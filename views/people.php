cb88fe1e9dcade95f4b3f5c60d3f54d6e277a505
Elvis Bui <elvisbui.ed@gmail.com>
2020-01-12 14:52:43 -0500

    Initial search by person name

--- views/people.php
+++ views/people.php
@@ -8,7 +8,8 @@
     <div class="container">
         <div class="text-center mt-3">
             <h1>People</h1>
-            <table class="table table-bordered mt-3">
+            <input type="text" id="search" onkeyup="filter()" placeholder="Search for names...">
+            <table id="table" class="table table-bordered mt-3">
                 <thead class="thead-dark">
                     <tr>
                         <th>People ID#</th>
@@ -35,6 +36,29 @@
         </div>
     </div>
 
+    <script>
+        function filter() {
+            // Declare variables
+            var input, filter, table, tr, td, i, txtValue;
+            input = document.getElementById("search");
+            filter = input.value.toUpperCase();
+            table = document.getElementById("table");
+            tr = table.getElementsByTagName("tr");
+
+            // Loop through all table rows, and hide those who don't match the search query
+            for (i = 0; i < tr.length; i++) {
+                td = tr[i].getElementsByTagName("td")[1];
+                if (td) {
+                    txtValue = td.textContent || td.innerText;
+                    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                        tr[i].style.display = "";
+                    } else {
+                        tr[i].style.display = "none";
+                    }
+                }
+            }
+        }
+    </script>
 <?php
     include '../partials/footer.php'
 ?>
