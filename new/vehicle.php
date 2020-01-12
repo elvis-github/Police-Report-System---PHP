da2f009a2ec9b03009945a5c20472223a0ec67c0
Elvis Bui <elvisbui.ed@gmail.com>
2020-01-12 18:21:56 -0500

    Initial vehicle page form

--- new/vehicle.php
+++ new/vehicle.php
@@ -9,10 +9,30 @@
     require_once '../connection/connection.php';
 ?>
     <div class="container">
-        <div class="text-center mt-3">
+        <div class="mt-3">
             <h1>Add New Vehicle</h1>
-            
-            <a class="btn btn-sm btn-primary mt-2" href="main.php">Go Back To Main</a>
+            <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="POST"> 
+                <div class="form-group">
+                    <label>Make & Model</label>
+                    <input type="text" name="vehicleType" class="form-control" value="">
+                </div>
+                <div class="form-group">
+                    <label>Colour</label>
+                    <input type="text" name="color" class="form-control">
+                </div>
+                <div class="form-group">
+                    <label>Licence</label>
+                    <input type="text" name="owner" class="form-control">
+                </div>
+                <div class="form-group">
+                    <label>Owner</label>
+                    <input type="text" name="owner" class="form-control">
+                </div>
+                <div class="form-group">
+                    <input type="submit" class="btn btn-primary" value="Submit">
+                    <a class="btn btn-link" href="../views/main.php">Cancel</a>
+                </div>
+            </form>
         </div>
     </div>
 
