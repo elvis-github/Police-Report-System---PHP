f1a5feeea4218ece05d0e2630d2b7634170a1287
Elvis Bui <elvisbui.ed@gmail.com>
2020-01-12 19:44:04 -0500

    Added function to add to database and linked vehicles to new vehicle

--- new/vehicle.php
+++ new/vehicle.php
@@ -7,29 +7,42 @@
     }
 
     require_once '../connection/connection.php';
+    if(isset($_POST["submit"])){
+        $sql = "INSERT INTO vehicle (Vehicle_type, Vehicle_colour, Vehicle_licence)
+        VALUES ('".$_POST["vehicleType"]."','".$_POST["color"]."','".$_POST["licence"]."')";
+        if($pdo->query($sql)){
+            echo '<h1>Inserted Successfully</h1>';
+            unset($pdo);
+            header("location: ../views/vehicles.php");
+            exit;
+        } else {
+            echo '<h1>An error has occurred</h1>';
+        }
+    }
+    unset($pdo);
 ?>
     <div class="container">
         <div class="mt-3">
             <h1>Add New Vehicle</h1>
-            <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="POST"> 
+            <form action="" method="POST"> 
                 <div class="form-group">
                     <label>Make & Model</label>
-                    <input type="text" name="vehicleType" class="form-control" value="">
+                    <input type="text" name="vehicleType" required="required" class="form-control" value="">
                 </div>
                 <div class="form-group">
                     <label>Colour</label>
-                    <input type="text" name="color" class="form-control">
+                    <input type="text" name="color" required="required" class="form-control">
                 </div>
                 <div class="form-group">
                     <label>Licence</label>
-                    <input type="text" name="owner" class="form-control">
+                    <input type="text" name="licence" required="required" class="form-control">
                 </div>
                 <div class="form-group">
                     <label>Owner</label>
                     <input type="text" name="owner" class="form-control">
                 </div>
                 <div class="form-group">
-                    <input type="submit" class="btn btn-primary" value="Submit">
+                    <input type="submit" class="btn btn-primary" name="submit" value="Submit">
                     <a class="btn btn-link" href="../views/main.php">Cancel</a>
                 </div>
             </form>
