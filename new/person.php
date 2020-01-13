b9de5f604cf655ab9b371817aa28c0811d8df0f2
Elvis Bui <elvisbui.ed@gmail.com>
2020-01-12 21:47:15 -0500

    Refactored previous html to fit person and linked main

--- new/person.php
+++ new/person.php
@@ -7,99 +7,83 @@
     }
 
     require_once '../connection/connection.php';
-    $vehicleTypeVal = $vehicleColorVal = $licenceErr = '';
+    $nameVal = $addrVal = $licenceErr = '';
 
-    if(isset($_POST["submit"])){
-        //Assign values for text inputs below
-        $vehicleTypeVal = $_POST["vehicleType"];
-        $vehicleColorVal = $_POST["color"];
+    // if(isset($_POST["submit"])){
+    //     //Assign values for text inputs below
+    //     $vehicleTypeVal = $_POST["vehicleType"];
+    //     $vehicleColorVal = $_POST["color"];
 
-        //Check if licence is correct format
-        $_POST["licence"] = strtoupper($_POST["licence"]);
-        if(verifyLicence($_POST["licence"])){
-            // SQL statement to insert into Vehicle Table
+    //     //Check if licence is correct format
+    //     $_POST["licence"] = strtoupper($_POST["licence"]);
+    //     if(verifyLicence($_POST["licence"])){
+    //         // SQL statement to insert into Vehicle Table
             
-            $sql = "INSERT INTO vehicle (Vehicle_type, Vehicle_colour, Vehicle_licence)
-            VALUES ('".$_POST["vehicleType"]."','".$_POST["color"]."','".$_POST["licence"]."')";
-            if($pdo->query($sql)){
-                // Once inserted, check if owner was set
-                if(isset($_POST["owner"])){
-                    // If Owner is set, add Owner and Vehicle ID to Ownership table
-                    echo '<h1>Owner Entered '. $_POST["owner"] . '</h1>';
+    //         $sql = "INSERT INTO vehicle (Vehicle_type, Vehicle_colour, Vehicle_licence)
+    //         VALUES ('".$_POST["vehicleType"]."','".$_POST["color"]."','".$_POST["licence"]."')";
+    //         if($pdo->query($sql)){
+    //             // Once inserted, check if owner was set
+    //             if(isset($_POST["owner"])){
+    //                 // If Owner is set, add Owner and Vehicle ID to Ownership table
+    //                 echo '<h1>Owner Entered '. $_POST["owner"] . '</h1>';
                     
-                    // Retrieve vehicle ID from added Vehicle
-                    $sql = 'SELECT Vehicle_ID from vehicle WHERE Vehicle_licence = :licence';
-                    $stmt = $pdo->prepare($sql);
-                    $stmt->bindParam(':licence', $paramLicence, PDO::PARAM_STR);
-                    $paramLicence = ($_POST["licence"]);
-                    $stmt->execute();
-                    $row = $stmt->fetch();
+    //                 // Retrieve vehicle ID from added Vehicle
+    //                 $sql = 'SELECT Vehicle_ID from vehicle WHERE Vehicle_licence = :licence';
+    //                 $stmt = $pdo->prepare($sql);
+    //                 $stmt->bindParam(':licence', $paramLicence, PDO::PARAM_STR);
+    //                 $paramLicence = ($_POST["licence"]);
+    //                 $stmt->execute();
+    //                 $row = $stmt->fetch();
 
-                    // Set vehicle ID as a variable
-                    $vehicleID = $row->Vehicle_ID;
-                    unset($stmt);
+    //                 // Set vehicle ID as a variable
+    //                 $vehicleID = $row->Vehicle_ID;
+    //                 unset($stmt);
 
-                    // Insert People ID and Vehicle ID to Ownership table
-                    $sql = "INSERT INTO ownership (People_ID, Vehicle_ID)
-                    VALUES ('".$_POST["owner"]."','".$vehicleID."')";
-                    $pdo->query($sql);
+    //                 // Insert People ID and Vehicle ID to Ownership table
+    //                 $sql = "INSERT INTO ownership (People_ID, Vehicle_ID)
+    //                 VALUES ('".$_POST["owner"]."','".$vehicleID."')";
+    //                 $pdo->query($sql);
                     
-                }
-                unset($pdo);
-                header("location: ../views/vehicles.php");
-                exit;
-            } 
-        }
-    }
+    //             }
+    //             unset($pdo);
+    //             header("location: ../views/vehicles.php");
+    //             exit;
+    //         } 
+    //     }
+    // }
 
-    function verifyLicence($str){
-        if(preg_match('/^[A-Z]{2}[0-9]{2}[A-Z]{3}$/', $str)){
-            $licenceErr = '';
-            return true;
-        } else {
-            if(strlen($str) != 7){
-               $GLOBALS['licenceErr'] = 'Licence length is incorrect. Must be 7 Characters in Length!'; // Length is incorrect
-               return false;
-            } else { 
-                $GLOBALS['licenceErr'] = 'Licence format is incorrect. Must be 2 Alpha characters, 2 Numbers, and 3 Alpha Characters.'; // Licence format is incorrect
-                return false;
-            }
-        }
-    }
+    // function verifyLicence($str){
+    //     if(preg_match('/^[A-Z]{2}[0-9]{2}[A-Z]{3}$/', $str)){
+    //         $licenceErr = '';
+    //         return true;
+    //     } else {
+    //         if(strlen($str) != 7){
+    //            $GLOBALS['licenceErr'] = 'Licence length is incorrect. Must be 7 Characters in Length!'; // Length is incorrect
+    //            return false;
+    //         } else { 
+    //             $GLOBALS['licenceErr'] = 'Licence format is incorrect. Must be 2 Alpha characters, 2 Numbers, and 3 Alpha Characters.'; // Licence format is incorrect
+    //             return false;
+    //         }
+    //     }
+    // }
     
 ?>
     <div class="container">
         <div class="mt-3">
-            <h1>Add New Vehicle</h1>
+            <h1>Add New Person</h1>
             <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="POST"> 
                 <div class="form-group">
-                    <label>Make & Model</label>
-                    <input type="text" name="vehicleType" required="required" class="form-control" value="<?php echo $vehicleTypeVal ?>">
+                    <label>Name</label>
+                    <input type="text" name="name" required="required" class="form-control" value="<?php echo $nameVal ?>">
                 </div>
                 <div class="form-group">
-                    <label>Colour</label>
-                    <input type="text" name="color" required="required" class="form-control" value="<?php echo $vehicleColorVal ?>">
+                    <label>Address</label>
+                    <input type="text" name="address" required="required" class="form-control" value="<?php echo $addrVal ?>">
                 </div>
                 <div class="form-group">
-                    <label>Licence</label>
+                    <label>Person's Licence</label>
                     <input type="text" name="licence" required="required" class="form-control">
                     <span class="d-block text-danger"><?php echo $licenceErr; ?></span>
-                </div>
-                <div class="form-group">
-                    <label>Owner</label><a class="ml-1 badge badge-success" href="../new/person.php">Add New Person</a>
-                    <select name="owner" class="d-block">
-                        <option selected>Choose...</option>
-                        <?php
-                            $stmt = $pdo->query("SELECT People_ID, People_name, People_licence
-                                                FROM people");
-                            while($row = $stmt->fetch()){
-                                echo '<option value="' . $row->People_ID.'">' . $row->People_name . ' (' . $row->People_licence . ')</option>';
-                            }
-                            unset($stmt);
-                            unset($pdo);
-                        ?>   
-                    </select>
-
                 </div>
                 <div class="form-group">
                     <input type="submit" class="btn btn-primary" name="submit" value="Submit">
